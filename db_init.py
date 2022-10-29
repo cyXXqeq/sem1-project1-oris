7b640ac23d01475651665e50d205c928b6190152
gaBUBUNts <89221310+gaBUBUNts@users.noreply.github.com>
2022-10-29 22:53:57 +0300

    add database initial

---
+++ db_init.py
@@ -0,0 +1,98 @@
+import psycopg2
+import json
+
+con = psycopg2.connect(
+    dbname='avito',
+    user='postgres',
+    password='29853461',
+    host='localhost',
+    port='5432'
+)
+
+cur = con.cursor()
+
+if __name__ == '__main__':
+    cur.execute(
+        '''create table users
+        (
+            name     varchar(255),
+            email    varchar(255) NOT NULL UNIQUE,
+            salt     varchar(40)  NOT NULL,
+            password varchar      NOT NULL,
+            id       integer      NOT NULL UNIQUE,
+            PRIMARY KEY (id)
+        );
+        
+        create table products
+        (
+            title       varchar(255) NOT NULL,
+            description text         NOT NULL,
+            cost        money        NOT NULL,
+            category    varchar      NOT NULL,
+            id          integer      NOT NULL UNIQUE,
+            PRIMARY KEY (id)
+        );
+        
+        create table orders
+        (
+            user_id integer NOT NULL,
+            summa   money   NOT NULL,
+            id      integer NOT NULL UNIQUE,
+            PRIMARY KEY (id),
+            CONSTRAINT fk_user
+                FOREIGN KEY (user_id)
+                    REFERENCES users (id)
+                    ON DELETE CASCADE
+        );
+        
+        create table purchases
+        (
+            product_id integer NOT NULL,
+            orders_id  integer NOT NULL,
+            CONSTRAINT fk_product
+                FOREIGN KEY (product_id)
+                    REFERENCES products (id)
+                    ON DELETE CASCADE,
+            CONSTRAINT fk_orders
+                FOREIGN KEY (orders_id)
+                    REFERENCES orders (id)
+                    ON DELETE CASCADE
+        );
+        
+        create table favourites
+        (
+            user_id    integer NOT NULL,
+            product_id integer NOT NULL,
+            CONSTRAINT fk_user
+                FOREIGN KEY (user_id)
+                    REFERENCES users (id)
+                    ON DELETE CASCADE,
+            CONSTRAINT fk_product
+                FOREIGN KEY (product_id)
+                    REFERENCES products (id)
+                    ON DELETE CASCADE
+        );
+        
+        create table basket
+        (
+            user_id    integer NOT NULL,
+            product_id integer NOT NULL,
+            CONSTRAINT fk_user
+                FOREIGN KEY (user_id)
+                    REFERENCES users (id)
+                    ON DELETE CASCADE,
+            CONSTRAINT fk_product
+                FOREIGN KEY (product_id)
+                    REFERENCES products (id)
+                    ON DELETE CASCADE
+        );'''
+    )
+    con.commit()
+
+    id_dict = {
+        'user': 0,
+        'product': 0,
+        'order': 0
+    }
+    with open('id_dict.json', 'w') as id_json:
+        json.dump(id_dict, id_json)
