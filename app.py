bbb9f90ba280388f0ddc93f82b185babb2717aaf
gaBUBUNts <89221310+gaBUBUNts@users.noreply.github.com>
2022-11-10 21:36:38 +0300

    add adverts page (main page) add advert page

--- app.py
+++ app.py
@@ -17,6 +17,36 @@ def about():
 
 @app.route('/adverts')
 def main():
+    adverts = Advert.get_all()
+    if session.get('user_id'):
+        user = User.get_all(id=session['user_id'])
+    else:
+        user = None
+    context = {
+        'adverts': adverts,
+        'user': user
+    }
+    return render_template('main.html', **context)
+
+
+@app.route('/advert/<int:id>/')
+def advert(id):
+    adv = Advert.get_all(id=id)
+    owner = User.get_all(id=adv.user_id).name
+    if session.get('user_id'):
+        user = User.get_all(id=session['user_id'])
+    else:
+        user = None
+    context = {
+        'advert': adv,
+        'user': user,
+        'owner': owner
+    }
+    return render_template('advert.html', **context)
+
+
+@app.route('/login')
+def login():
     pass
 
 
